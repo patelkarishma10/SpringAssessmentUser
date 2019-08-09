728b112ec77d1e97a1412451dfb9a325a960d17c
patelkarishma10 <patelkarishma10@gmail.com>
2019-08-09 13:07:36 +0100

    testing above 80%

--- src/main/java/com/bae/util/Constants.java
+++ src/main/java/com/bae/util/Constants.java
@@ -2,6 +2,8 @@ package com.bae.util;
 
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import com.bae.entity.User;
@@ -12,4 +14,7 @@ public class Constants {
 	public static final User MOCK_USER_OBJECT = new User(1, "user1", 1);
 	public static final User MOCK_USER_OBJECT2 = new User(2, "user2", 2);
 	public static final Optional<User> MOCK_USER_OBJECT3 = Optional.of(MOCK_USER_OBJECT);
+//	public static final ResponseEntity<String> MOCK_USER_OBJECT4 = restTemplate.exchange("http://localhost:8089/search/find/" + searchTerm,
+//			HttpMethod.GET, null, String.class);
+	public static final ResponseEntity<String> MOCK_USER_OBJECT4 = new ResponseEntity<String>("search", HttpStatus.OK);
 }
--- src/test/java/com/bae/logic/UserServiceTest.java
+++ src/test/java/com/bae/logic/UserServiceTest.java
@@ -8,7 +8,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import com.bae.repository.UserRepository;
 import com.bae.util.Constants;
@@ -23,6 +26,12 @@ public class UserServiceTest {
 	@Mock
 	UserRepository repository;
 
+	@Mock
+	RestTemplate restTemplate;
+
+	@Mock
+	JmsTemplate jmsTemplate;
+
 	@Test
 	public void getAUserTest() {
 		Mockito.when(repository.findById((long) 3)).thenReturn(Constants.MOCK_USER_OBJECT3);
@@ -35,6 +44,18 @@ public class UserServiceTest {
 		Mockito.when(repository.save(Constants.MOCK_USER_OBJECT)).thenReturn(Constants.MOCK_USER_OBJECT);
 		assertEquals(Constants.MOCK_USER_OBJECT, service.createUser(Constants.MOCK_USER_OBJECT));
 		Mockito.verify(repository).save(Constants.MOCK_USER_OBJECT);
+	}
+
+	@Test
+	public void searchTest() {
+
+		Mockito.when(repository.findById((long) 1)).thenReturn(Constants.MOCK_USER_OBJECT3);
+		String searchTerm = "search";
+		Mockito.when(restTemplate.exchange("http://localhost:8089/search/find/" + searchTerm, HttpMethod.GET, null,
+				String.class)).thenReturn(Constants.MOCK_USER_OBJECT4);
+		String searchResult = Constants.MOCK_USER_OBJECT4.getBody();
+		assertEquals(searchResult, service.search(1, searchTerm));
+		Mockito.verify(repository).findById((long) 1);
 	}
 
 }
