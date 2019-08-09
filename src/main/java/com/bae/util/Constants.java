a4d7acb2abe40971f4d230d127a020ecd397073a
patelkarishma10 <patelkarishma10@gmail.com>
2019-08-09 13:48:32 +0100

    removed commented code

--- src/main/java/com/bae/util/Constants.java
+++ src/main/java/com/bae/util/Constants.java
@@ -14,7 +14,5 @@ public class Constants {
 	public static final User MOCK_USER_OBJECT = new User(1, "user1", 1);
 	public static final User MOCK_USER_OBJECT2 = new User(2, "user2", 2);
 	public static final Optional<User> MOCK_USER_OBJECT3 = Optional.of(MOCK_USER_OBJECT);
-//	public static final ResponseEntity<String> MOCK_USER_OBJECT4 = restTemplate.exchange("http://localhost:8089/search/find/" + searchTerm,
-//			HttpMethod.GET, null, String.class);
 	public static final ResponseEntity<String> MOCK_USER_OBJECT4 = new ResponseEntity<String>("search", HttpStatus.OK);
 }
